3882eb041aef6f23a39ed8d4de050734975d33f8
Salman <salman315@gmail.com>
2013-07-15 18:02:21 +0530

    Testing web.js with a string contained using readFileSync()

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var localText = fs.readFileSync('./index.html', 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(localText);
 });
 
 var port = process.env.PORT || 5000;
